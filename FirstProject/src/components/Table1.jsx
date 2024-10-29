c130191414a58b2cd7a8db20410105d85cd35d1c
Dhvanibarot5 <dhvanibarot5@gmail.com>
2024-10-30 01:18:40 +0530

    Start of React

---
+++ FirstProject/src/components/Table1.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+// #region constants
+
+// #endregion
+
+// #region styled-components
+
+// #endregion
+
+// #region functions
+
+// #endregion
+
+// #region component
+const propTypes = {};
+
+const defaultProps = {};
+
+/**
+ * 
+ */
+const Table1 = () => {
+    return <div></div>;
+}
+
+Table1.propTypes = propTypes;
+Table1.defaultProps = defaultProps;
+// #endregion
+
+export default Table1;
