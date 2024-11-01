c88406154529ff4d84d46b55d80bd024bf6f5f94
Dhvanibarot5 <dhvanibarot5@gmail.com>
2024-11-01 17:25:27 +0530

    increment done in counter

--- Counter/components/Counter.jsx
+++ Counter/components/Counter.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Counter() {
-  return <div>Counter</div>;
+  const [count, setCount] = useState(0);
+  //   console.log("count", count);
+
+  const decrement = () => {
+    console.log("hello");
+  };
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="flex">
+      <button onClick={() => decrement()}>Decrement</button>
+      <h1>{count}</h1>
+      <button onClick={() => increment()}>Increment</button>
+    </div>
+  );
 }
