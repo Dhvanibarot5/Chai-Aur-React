3c3f0c06135f97477c66a749b4c6e02aaa8be4a4
Dhvanibarot5 <dhvanibarot5@gmail.com>
2024-11-04 22:46:39 +0530

    header section done

---
+++ hospital/src/components/Buttons.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Buttons() {
+  return <button style={{ backgroundColor: "#385182", padding: "9px" }} className="rounded-lg text-white">Make Appointment</button>;
+}
--- hospital/src/components/Header.jsx
+++ hospital/src/components/Header.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import Logo from "../../public/Logo.png";
-
+import Buttons from "./Buttons";
 export default function Header() {
   return (
     <section>
       <div className="container mx-auto">
-        <nav className="flex justify-between items-center h-10 border p-5 sticky top-0 left-0 right-0">
+        <nav className="flex justify-between items-center h-10 p-5 sticky top-0 left-0 right-0 mt-5">
           <img src={Logo} alt="logo" />
           <ul className="flex">
             <li className="p-4">Home</li>
             <li className="p-4">About</li>
+            <li className="p-4">
+              Medical Services <i class="fa-solid fa-angle-down"></i>
+            </li>
+            <li className="p-4">Career</li>
+            <li className="p-4">News</li>
             <li className="p-4">Contact Us</li>
-            <li className="p-4">More Info</li>
           </ul>
+          <div className="icon">
+            <i class="fa-regular fa-circle-user text-2xl pe-5"></i>
+            <Buttons />
+          </div>
         </nav>
       </div>
     </section>
