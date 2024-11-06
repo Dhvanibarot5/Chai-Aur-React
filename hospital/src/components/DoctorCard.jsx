bf043de6e9d66f8b084f4ab869aabbc88a438357
Dhvanibarot5 <dhvanibarot5@gmail.com>
2024-11-06 19:45:45 +0530

    doctor section done

--- hospital/src/components/DoctorCard.jsx
+++ hospital/src/components/DoctorCard.jsx
@@ -5,11 +5,11 @@ import Buttons from "./Buttons";
 export default function DoctorCard() {
   return (
     <div className="w-1/2 p-3 mt-20">
-      <div className="bg-[#385182] w-[550px] h-[211px] rounded-xl relative">
-        <img src={Heroimg3} className="absolute -top-14 -left-7 " />
-        <div className="flex-col ">
-          <h1 className="text-white">Dr.Christopher Nicks</h1>
-          <p className="text-[#d1cece]">MBBS,FCPS - Cardiologist</p>
+      <div className="bg-[#385182] w-[525px] h-[211px] rounded-xl relative">
+        <img src={Heroimg3} className="absolute -top-14 -left-6" />
+        <div className="absolute right-16 top-14">
+          <h1 className="text-white text-2xl mb-2">Dr.Christopher Nicks</h1>
+          <p className="text-[#d1cece] mb-4">MBBS,FCPS - Cardiologist</p>
           <Buttons />
         </div>
       </div>
--- hospital/src/components/OurDoctors.jsx
+++ hospital/src/components/OurDoctors.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import DoctorCard from "./DoctorCard";
 import Heroimg3 from "../../public/Heroimg3.png";
+import Leftarrowimg from "../../public/Leftarrowimg.png";
+import Whitearrowimg from "../../public/Whitearrowimg.png";
+import Leaf from "../../public/Leaf.png";
+import Docbgimg from "../../public/Docbgimg.png";
 
 export default function OurDoctors() {
   return (
     <section className="py-24">
-      <div className="container mx-auto">
+      <div className="container mx-auto relative">
         <div className="">
           <h1 className="text-[#6285CA] text-xl font-medium mb-3">Our Doctors</h1>
-          <h2 className="text-4xl">Meet Our Professional Expert <br /> Doctor</h2>
+          <h2 className="text-4xl">
+            Meet Our Professional Expert <br /> Doctor
+          </h2>
         </div>
         <div className="flex">
-          <DoctorCard />
-          <DoctorCard />
+          <div className="w-1/2">
+            <DoctorCard />
+          </div>
+          <div className="w-1/2">
+            <DoctorCard />
+            <h1 className="mb-5 text-center text-3xl text-gray-300">OUR DOCTOR</h1>
+            <div className="flex">
+              <img src={Leftarrowimg} alt="" />
+              <img src={Whitearrowimg} className="ms-3 shadow-xl rounded-full" />
+            </div>
+          </div>
+          <img src={Leaf} className="absolute right-28 bottom-0" />
+          <img src={Docbgimg} className="absolute right-0 top-0" />
         </div>
       </div>
     </section>
