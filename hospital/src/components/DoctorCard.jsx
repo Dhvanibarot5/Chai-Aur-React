3b4cb47563565cc3d160ac2189ae9f9587f8aac8
Dhvanibarot5 <dhvanibarot5@gmail.com>
2024-11-06 07:38:53 +0530

    created half doctor section

--- hospital/src/components/DoctorCard.jsx
+++ hospital/src/components/DoctorCard.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Heroimg3 from "../../public/Heroimg3.png";
+import Buttons from "./Buttons";
 
 export default function DoctorCard() {
   return (
-    <div className="border border-green-500 w-1/2 p-3">
-      <div className="border border-blue-400">DoctorCard</div>
+    <div className="w-1/2 p-3 mt-20">
+      <div className="bg-[#385182] w-[550px] h-[211px] rounded-xl relative">
+        <img src={Heroimg3} className="absolute -top-14 -left-7 " />
+        <div className="flex-col ">
+          <h1 className="text-white">Dr.Christopher Nicks</h1>
+          <p className="text-[#d1cece]">MBBS,FCPS - Cardiologist</p>
+          <Buttons />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- hospital/src/components/OurDoctors.jsx
+++ hospital/src/components/OurDoctors.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import DoctorCard from "./DoctorCard";
+import Heroimg3 from "../../public/Heroimg3.png";
 
 export default function OurDoctors() {
   return (
-    <section>
+    <section className="py-24">
       <div className="container mx-auto">
-        OurDoctors
+        <div className="">
+          <h1 className="text-[#6285CA] text-xl font-medium mb-3">Our Doctors</h1>
+          <h2 className="text-4xl">Meet Our Professional Expert <br /> Doctor</h2>
+        </div>
         <div className="flex">
           <DoctorCard />
           <DoctorCard />
