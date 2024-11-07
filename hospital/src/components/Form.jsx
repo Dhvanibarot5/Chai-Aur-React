202eace02842090f30d4a23432fd219315a5465c
Dhvanibarot5 <dhvanibarot5@gmail.com>
2024-11-07 19:46:19 +0530

    Help section done

--- hospital/src/components/Form.jsx
+++ hospital/src/components/Form.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import Buttons from "./Buttons";
 
 export default function Form() {
   return (
-    <div>Form</div>
-  )
+    <div>
+      <form action="" className="bg-white rounded-3xl py-3">
+        <div className="p-5">
+          <h1 className="text-3xl font-medium">How Can We Help</h1>
+          <p className="text-[#888888] my-4">
+            Please feel welcome to contact our friendly reception staff with any <br /> general or medical enquiry. Our doctors will receive or return
+            any urgent calls.
+          </p>
+          <div className="flex justify-between mb-5">
+            <input type="text" placeholder="Name" className="bg-[#F5F6F9] py-3 px-9 rounded-xl me-3" />
+            <input type="text" placeholder="Email" className="bg-[#F5F6F9] py-3 px-9 rounded-xl" />
+          </div>
+          <div className="flex justify-between">
+            <input type="text" placeholder="Phone Number" className="bg-[#F5F6F9] py-3 px-9 rounded-xl" />
+            <input type="text" placeholder="Subject" className="bg-[#F5F6F9] py-3 px-9 rounded-xl" />
+          </div>
+          <textarea name="message" id="" placeholder="Message" rows={5} cols={69} className="bg-[#F5F6F9] rounded-xl py-4 px-9 my-5"></textarea>
+          <Buttons />
+        </div>
+      </form>
+    </div>
+  );
 }
--- hospital/src/components/Help.jsx
+++ hospital/src/components/Help.jsx
@@ -1,17 +1,38 @@
 import React from "react";
 import Form from "./Form";
+import vector from "../../public/Vector.png";
+import vector2 from "../../public/Vector2.png";
 
 export default function Help() {
   return (
-    <section>
-      <div className="container mx-auto border border-green-500">
-        Help
+    <section className="py-24 bg-[#F5F6F9]">
+      <div className="container mx-auto relative">
         <div className="flex">
-          <div className="border border-blue-500 m-2 w-1/2">content</div>
-          <div className="border border-blue-500 m-2 w-1/2">
+          <div className="m-2 w-1/2">
+            <h2 className="text-[#6285CA] mb-3">Help</h2>
+            <h1 className="text-4xl font-semibold mb-5">
+              A Dialysis Patient’s Life Is <br /> Hard. Laugh Harder.
+            </h1>
+            <h3 className="font-medium ">Our support services inclide:</h3>
+            <p className="text-[#888888] my-3">
+              Today the hospital is recognised as a world-renowned institution, <br /> not only providing outstanding care and treatment, our goal is
+              to <br />
+              deliver quality care in a respectful & compassionate manner. We <br /> strive to be the first and best choice for healthcare.
+            </p>
+            <ul className="text-[#888888] list-disc ms-5">
+              <li>A family history of stone disease</li>
+              <li>Stones caused by an infection in your urinary system</li>
+              <li>A genetic condition which makes you prone to forming stones</li>
+              <li>A cyst or a condition called calyceal diverticulum</li>
+              <li>Swelling in one of your ureters, called ureterocele</li>
+            </ul>
+          </div>
+          <div className="m-2 w-1/2">
             <Form />
           </div>
         </div>
+        <img src={vector} className="absolute -top-16 left-96" />
+        <img src={vector2} className="absolute bottom-12 left-[510px]" />
       </div>
     </section>
   );
